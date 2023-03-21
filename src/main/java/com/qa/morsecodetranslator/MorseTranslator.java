1f4fc566bb4e4d600c2572e804544a9c32acce8d
Admin <Admin@192.168.1.100>
2023-03-21 15:33:19 +0000

    init commit

---
+++ src/main/java/com/qa/morsecodetranslator/MorseTranslator.java
@@ -0,0 +1,10 @@
+package com.qa.morsecodetranslator;
+
+public class MorseTranslator {
+
+	public String translate(String morse) {
+		String s = "";
+		return s;
+	}
+
+}
---
+++ src/main/java/com/qa/morsecodetranslator/Runner.java
@@ -0,0 +1,9 @@
+package com.qa.morsecodetranslator;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+	}
+
+}
