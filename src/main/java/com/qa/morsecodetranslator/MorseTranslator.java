bb913c28792b4b41d9298f2b4ebb8764c25f42cc
Admin <Admin@192.168.1.100>
2023-03-21 16:10:07 +0000

    Added morse alphabet

--- src/main/java/com/qa/morsecodetranslator/MorseTranslator.java
+++ src/main/java/com/qa/morsecodetranslator/MorseTranslator.java
@@ -1,9 +1,35 @@
 package com.qa.morsecodetranslator;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class MorseTranslator {
+
+	private Map<String, Character> morseAlphabet = new HashMap<String, Character>();
+	private String[] morseChars = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
+			".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--",
+			"--.." };
+
+	public MorseTranslator() {
+		int n = 0;
+		for (int i = 'A'; i <= 'Z'; i++) {
+			morseAlphabet.put(morseChars[n], (char) i);
+			n++;
+		}
+	}
+
+	public void printMorseAlphabet() {
+		for (Entry<String, Character> i : morseAlphabet.entrySet()) {
+			System.out.println(i);
+		}
+	}
 
 	public String translate(String morse) {
 		String s = "";
+		// Split string on / (new word)
+		String[] words = morse.split("/");
+		// Split words on spaces (new character)
 		return s;
 	}
 
--- src/main/java/com/qa/morsecodetranslator/Runner.java
+++ src/main/java/com/qa/morsecodetranslator/Runner.java
@@ -3,6 +3,7 @@ package com.qa.morsecodetranslator;
 public class Runner {
 
 	public static void main(String[] args) {
+		MorseTranslator translator = new MorseTranslator();
 
 	}
 
